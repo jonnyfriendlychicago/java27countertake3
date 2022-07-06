d930ed02698b3bcfe0900feed4c1734327fbd924
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-07-06 16:11:18 -0500

    first commit - template java app running

---
+++ src/main/java/com/jonfriend/java27countertake3/Java27countertake3Application.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java27countertake3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java27countertake3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java27countertake3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonfriend/java27countertake3/Java27countertake3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java27countertake3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java27countertake3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
